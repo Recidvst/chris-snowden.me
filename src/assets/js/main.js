5d9e25b548c260d0b338b6eaccf5d7d6f3548389
Chris Snowden <cdsnowden@gmail.com>
2021-04-10 19:25:02 +0100

    feat(headings): add scrambler to subtitles and align timings with title

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,19 +8,55 @@ import '../styles/main.scss'; // eslint-disable-line import-order-aesthetic/orde
 document.addEventListener("DOMContentLoaded", function() {
   // trigger quicklink (prefetch URLs)
   listen();
+
   // start the neon lines in background
   linesStart();
+
   // set off the scrambling function and flicker function on the main page title
-  const mainTitleSelector = '.title.neon-title';
-  const mainTitle = document.querySelector(mainTitleSelector);
+  // set off the scrambling function for the subordinate subtitles via recursion
+  const mainTitle = document.querySelector('.page-title-container .title.neon-title');
+  const subTitles = [...document.querySelectorAll('.page-title-container .page-title-container__subtitles *')];
+
+  if (subTitles && subTitles.length > 0) {
+    subTitles.map( (item, index) => item.classList.add(`neon-subtitle__${index}`, 'hidden'));
+  }
+
+  let recursiveScramblerIndex = 0;
+  function recursiveScrambler(limit, curIndex) {
+    if (curIndex >= limit) {
+      return;
+    }
+    let ind = curIndex;
+
+    scramblerTrigger({
+      target: `.neon-subtitle__${ind}`,
+      random: [750, 1500],
+      speed: 80,
+      beforeAll: function() {
+        document.querySelector(`.neon-subtitle__${ind}`).classList.remove('hidden');
+      },
+      afterAll: function() {
+        ind++;
+        recursiveScrambler(subTitles.length, ind);
+      },
+    });
+  }
+
   if (mainTitle) {
     scramblerTrigger({
       target: '.title.neon-title',
+      random: [3000, 6000],
+      speed: 100,
       afterAll: function() {
         flickerAction(mainTitle);
       },
     });
   }
+  // trigger the subtitles
+  if (subTitles && subTitles.length > 0) {
+    recursiveScrambler(subTitles.length, recursiveScramblerIndex)
+  }
+
   // set off the flicker function at 20sec intervals and watch for hovers to trigger it manually
   // these are applied to all items with the 'neon-title' class
   regularFlicker(20000);
