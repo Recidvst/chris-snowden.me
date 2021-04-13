080dec8404c2b6c9024875befa31de4add673c36
Chris Snowden <cdsnowden@gmail.com>
2021-04-13 19:31:18 +0100

    refactor(pwa): only use the service worker for prod build past v1.0.0

--- src/_data/environment.js
+++ src/_data/environment.js
@@ -2,4 +2,5 @@ module.exports = {
   isDev: process.env.ELEVENTY_ENV === 'development' || process.env.NODE_ENV === 'development',
   isProd: process.env.ELEVENTY_ENV === 'production' || process.env.NODE_ENV === 'production',
   eleventyEnv: process.env.ELEVENTY_ENV,
+  siteVersion: process.env.SITE_VERSION || 0,
 };
