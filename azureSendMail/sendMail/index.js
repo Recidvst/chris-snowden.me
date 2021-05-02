65e8bcd877da580e6ea99d98b91d4d31a6ff09d1
Chris Snowden <cdsnowden@gmail.com>
2021-05-02 18:26:40 +0100

    feat(azure): add error serialisation to return better errors from function app

--- azureSendMail/sendMail/index.js
+++ azureSendMail/sendMail/index.js
@@ -1,29 +1,34 @@
 module.exports = async function (context, req) {
 
+  // import
+  const {serializeError} = require('serialize-error');
+
   // env vars
   const MAILUSR = process.env["GMAILACCOUNT"] || false;
   const MAILPWD = process.env["GMAILPASSWORD"] || false;
   const CCEMAIL = process.env["CCADDRESS"] || false;
+  console.log(MAILUSR, MAILPWD, CCEMAIL);
 
   const sendMail = require('./sendMail.js');
 
   // get data from POST request
   let body = JSON.stringify(context.req.body);
   let data = JSON.parse(body);
+  console.log(body, data);
 
   // init status var
   let mailStatus;
 
   // throw errors if req body missing/badly formatted
-  if (!body || !data || typeof data !== 'object') {
+  if (!MAILUSR || !MAILPWD) {
+    mailStatus = Error(`Nodemailer send failed. Reason: failed to provide valid SMTP address and password. At: ${new Date().toISOString()}\r\n`);
+  }
+  else if (!body || !data || typeof data !== 'object') {
     mailStatus = Error(`Nodemailer send failed. Reason: request failed at data parse stage. Did you send JSON and set content-type header? At: ${new Date().toISOString()}\r\n`);
   }
   // throw errors if missing details
   else if (!data || !data.message || !data.email || !data.name) {
     mailStatus = Error(`Nodemailer send failed. Reason: failed to pass subject and/or message. At: ${new Date().toISOString()}\r\n`);
-  }
-  else if (!MAILUSR || !MAILPWD) {
-    mailStatus = Error(`Nodemailer send failed. Reason: failed to provide valid SMTP address and password. At: ${new Date().toISOString()}\r\n`);
   }
   else {
     // trigger mail send
@@ -53,7 +58,9 @@ module.exports = async function (context, req) {
 
   context.res = {
     status: responseStatus,
-    body: responseMessage,
+    body: {
+      result: serializeError(responseMessage),
+    },
   };
 
 }
