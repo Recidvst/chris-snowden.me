3337187544934409663b6170a409bb8a81e9b5db
Chris Snowden <cdsnowden@gmail.com>
2021-05-08 19:07:44 +0100

    feat(cache): use 11ty cache plugin to fetch google font and avoid a css network request

---
+++ src/_data/googleFont.js
@@ -0,0 +1,22 @@
+const Cache = require("@11ty/eleventy-cache-assets");
+
+module.exports = async function() {
+  try {
+    let url = "https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap";
+    let fontCSS = await Cache(url, {
+      duration: "1y",
+      type: "text",
+      fetchOptions: {
+        headers: {
+          "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+        },
+      },
+    });
+    let htmlStart = '<style>';
+    let htmlEnd = '</style>';
+    return htmlStart + fontCSS + htmlEnd;
+  }
+  catch {
+    return false;
+  }
+};
