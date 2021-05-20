f3673f2ba0d926bf1bda76bab3a11773fc817fbd
Chris Snowden <cdsnowden@gmail.com>
2021-05-20 08:18:59 +0100

    fix(netlify): allow unsafe csp header for now, will use hashes later

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,10 +3,10 @@ module.exports = {
   parserPreset: 'conventional-changelog-conventionalcommits',
   rules: {
     'body-leading-blank': [1, 'always'],
-    'body-max-line-length': [2, 'always', 100],
+    'body-max-line-length': [2, 'always', 200],
     'footer-leading-blank': [1, 'always'],
-    'footer-max-line-length': [2, 'always', 100],
-    'header-max-length': [2, 'always', 100],
+    'footer-max-line-length': [2, 'always', 200],
+    'header-max-length': [2, 'always', 200],
     'subject-case': [
       1,
       'never',
@@ -37,6 +37,6 @@ module.exports = {
     ],
     'scope-case': [0],
     'scope-empty': [1, 'never'],
-    'scope-enum': [0]
-  }
+    'scope-enum': [0],
+  },
 };
