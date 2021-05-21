0969160be37802932da8691584cbcc07f081bd62
Chris Snowden <cdsnowden@gmail.com>
2021-05-21 21:21:02 +0100

    feat(sensors): add an animated ghost svg, triggered when dark or after 9pm

--- src/assets/js/ambientLightController.js
+++ src/assets/js/ambientLightController.js
@@ -2,7 +2,7 @@
 export default function() {
 
   if ( 'AmbientLightSensor' in window || window.AmbientLightSensor ) {
-    console.log('ambient light sensor available');
+    // console.log('ambient light sensor available');
 
     // create sensor
     const sensor = new AmbientLightSensor();
@@ -10,21 +10,32 @@ export default function() {
 
     // catch errors
     sensor.addEventListener('error', error => {
-      console.error(error);
+      // console.error(error);
     });
 
     // watch for illumination change
     sensor.addEventListener('reading', () => {
       const illuminance = sensor.illuminance;
+      const ghost = document.getElementsByClassName('ghost')[0];
 
       // add/remove classes depending on illumination levels
       if (illuminance < 20) {
         document.body.classList.remove('light');
         document.body.classList.add('dark');
+        if (ghost) {
+          setTimeout( function() {
+            ghost.classList.add('active');
+          }, 1500);
+        }
       }
       else if (illuminance > 30) {
         document.body.classList.remove('dark');
         document.body.classList.add('light');
+        if (ghost) {
+          setTimeout( function() {
+            ghost.classList.remove('active');
+          }, 1500);
+        }
       }
     });
   }
