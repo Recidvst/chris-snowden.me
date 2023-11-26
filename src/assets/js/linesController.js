e9dbaea9a3901548af86809e352dd713ab978872
Chris Snowden <cdsnowden@gmail.com>
2023-11-26 15:59:08 +0000

    refactor(content): fiddle with lines and ghost a little

--- src/assets/js/linesController.js
+++ src/assets/js/linesController.js
@@ -2,8 +2,13 @@
 export function randColour() { // util fn to randomise icon colour
   let col = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
   while ( col === '#000000' ) col = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
-  return col;
+  return hex2rgba(col, 0.5);
 }
+
+export const hex2rgba = (hex, alpha = 1) => {
+  const [r, g, b] = hex.match(/\w\w/g).map(x => parseInt(x, 16));
+  return `rgba(${r},${g},${b},${alpha})`;
+};
 
 export function triggerDraw(path, draw = true) {
   if (path === undefined) return false; // check for path
@@ -15,7 +20,7 @@ export function triggerDraw(path, draw = true) {
   if (draw) { // draw in
     path.style.strokeDashoffset = 0;
     path.style.stroke = `${randColour()}`; // colour change
-    setTimeout( (e) => {
+    setTimeout( () => {
       path.style.strokeDashoffset = length;
       triggerDraw(path, false); // recusively call self
     }, duration);
@@ -23,7 +28,7 @@ export function triggerDraw(path, draw = true) {
   else { // draw out
     path.style.strokeDashoffset = length;
     path.style.stroke = `${randColour()}`; // colour change
-    setTimeout( (e) => {
+    setTimeout( () => {
       path.style.strokeDashoffset = 0;
       triggerDraw(path, true); // recusively call self
     }, duration);
@@ -33,25 +38,13 @@ export function triggerDraw(path, draw = true) {
 export function linesStart() {
   const svgPaths = [...document.querySelectorAll('#svgLinesContainer path')];
   if (svgPaths && Array.isArray(svgPaths)) {
-    // only do the animation on desktop - too resource heavy
-    // if (window.matchMedia("(min-width: 768px)").matches) { // TODO: remove this? is it OK on mobile now?
-    svgPaths.forEach( (path) => {
-      // get path length and init stroke. Starts offest to hide lines
-      let length = path.getTotalLength();
+    svgPaths.forEach((path) => {
       path.style.strokeDasharray = length;
-      path.style.strokeDashoffset = length;
       path.style.strokeWidth = '2px';
-      setTimeout( (e) => { // trigger draw fn
+      setTimeout(() => {
         triggerDraw(path, true);
-      }, 50);
+      }, 100);
     });
-    // }
-    // else {
-    //   svgPaths.forEach( (path) => {
-    //     path.style.strokeDashoffset = 0;
-    //     path.style.stroke = `${randColour()}`; // colour change
-    //     path.style.strokeWidth = '2px';
-    //   });
-    // }
   }
 }
+
